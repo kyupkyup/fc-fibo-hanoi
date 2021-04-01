d323e614dce1abbb17189acd8b45d21e4fc3cb70
kyupkyup <rudduqdl@naver.com>
2021-04-01 16:33:23 +0900

    feat : fibo-memo test over

--- fibo-memo.py
+++ fibo-memo.py
@@ -1,9 +1,18 @@
 
-def fibo(num):
+def fibo(num, memo):
+    if num == 1:
+        memo[num] = 1
+        return 1
+    elif num == 2:
+        memo[num] = 1
+        return 1
+    elif memo[num] != 0:
+        return memo[num]
+    else:
+        return fibo(num-2, memo) + fibo(num-1, memo)
 
-    pass
 
-
-if __name__ == __"main"__:
-    fibo(10)
-    print("init")
+if __name__ == "__main__":
+    x = 10
+    memo = [0] * (x+1)
+    print(fibo(x, memo))
