036ee9a9ee7b5c2045b64a40f155dee5949d5c50
chacha912 <dkwkdnwk912@gmail.com>
2021-04-01 16:32:49 +0900

    feat: Complete fibonacci with recursion

--- fibo.py
+++ fibo.py
@@ -1,6 +1,10 @@
 def fibonacci(num):
-    return num
+    if num <= 2:
+        return 1
+
+    return fibonacci(num - 1) + fibonacci(num - 2)
 
 if __name__ == '__main__':
-    fibonacci(10)
-    print('it works')
+    user_num = int(input('Type number : '))
+    for i in range(1, user_num + 1):
+        print(fibonacci(i), end=' ' )
